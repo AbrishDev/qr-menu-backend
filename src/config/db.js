c906742c295f1a7a5a6bff89a90179b50c6e3645
abrham-desalegn <abrhamdesalegn961@gmail.com>
2025-02-21 20:18:12 +0300

    just test the api's

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "./.env" });
 
-console.log("MONGO_URI:", process.env.MONGO_URI);
+// console.log("MONGO_URI:", process.env.MONGO_URI);
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
         });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`MongoDB Connected`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
